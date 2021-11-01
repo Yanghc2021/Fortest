e071f81ed8ee13e13cedd18c71ff67f8455ad8b7
Yanghc2021 <huichao.yang@thundersoft.com>
2021-11-01 15:06:34 +0800

    Remove Mycar

--- Untitled1/MyCar/mycar_Vehicle/Application/configs/platforms/android/src/com/rightware/kanzi/mycarvehicle/MycarVehicle.java
+++
@@ -1,9 +0,0 @@
-package com.rightware.kanzi.mycarvehicle;
-
-import com.rightware.kanzi.*;
-
-public class MycarVehicle extends KanziActivity
-{
-    private static final String TAG = "MycarVehicle";
-
-}
