4e157abcba8903758fd5962e123995426805ca4c
Yanghc2021 <huichao.yang@thundersoft.com>
2021-11-01 15:04:40 +0800

    Add Vehicle

---
+++ Untitled1/MyCar/mycar_Vehicle/Application/configs/platforms/android/src/com/rightware/kanzi/mycarvehicle/MycarVehicle.java
@@ -0,0 +1,9 @@
+package com.rightware.kanzi.mycarvehicle;
+
+import com.rightware.kanzi.*;
+
+public class MycarVehicle extends KanziActivity
+{
+    private static final String TAG = "MycarVehicle";
+
+}
